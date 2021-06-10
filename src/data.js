800d534360884438f0696248b522f07ccc4c3c02
saulokirchmaier <saulokirchmaier@gmail.com>
2021-06-10 12:01:22 -0300

    fix data, add youchef-app, and add click me in cards

--- src/data.js
+++ src/data.js
@@ -3,19 +3,21 @@ export const data = [
     type: 'React',
     projects: [
       {
+        title: 'YouChef App',
+        desciption: 'Aplicação mobile de receitas, é possível visualizar receitas de comidas e bebidas diversas, realizar buscas, favoritar, iniciar as receitas e visualizar as receitas feitas. Para melhor visualização utilize um celular.',
+        technologies: ['React', 'Redux', 'HTML', 'CSS'],
+        url: 'https://saulokirchmaier.github.io/youchef-app/#/',
+        thumbnail: 'https://i.ibb.co/hXXMW7j/You-Chef-app.png',
+        visit: "Acesse o site!"
+      },
+      {
         title: 'Github Search',
         desciption: 'Aplicação de busca de usuários do Github,  mostra os 5 primeiros usuários corespondentes e 3 repositórios mais atualizados do usuário selecionado.',
         technologies: ['React', 'Redux', 'HTML', 'CSS'],
         url: 'https://github-search-users-repos.herokuapp.com/',
         thumbnail: 'https://i.ibb.co/KWV2tjZ/github-search.png',
-      },
-      {
-        title: 'Lista de tarefas',
-        desciption: 'Desenvolvido para o curso da Tybe, esse porjeto é um to-do list de browser. Pode-se criar tarefas, marca-las como feitas, apagar e salvar localmente.',
-        technologies: ['JavaScript', 'HTML', 'CSS'],
-        url: 'https://saulokirchmaier.github.io/todo_list/',
-        thumbnail: 'https://i.ibb.co/C0zwqGp/to-do-list.png',
-      },
+        visit: "Acesse o site!"
+      }
     ]
   },
   {
@@ -23,38 +25,43 @@ export const data = [
     projects: [
       {
         title: 'Facebook',
-        desciption: 'Desenvolvido para o curso da Tybe, foi recriado a página de login antiga do facebook. Desenvolvida em dupla, este projeto foi bem desafiador e muito produtivo.',
+        desciption: 'Desenvolvido para o curso da Trybe, foi recriado a página de login antiga do facebook. Desenvolvida em dupla, este projeto foi bem desafiador e muito produtivo.',
         technologies: ['JavaScript', 'HTML', 'CSS'],
         url: 'https://saulokirchmaier.github.io/facebook_signup/',
         thumbnail: 'https://i.ibb.co/5BYTLzt/facebook-login.png',
+        visit: "Acesse o site!"
       },
       {
         title: 'Lista de tarefas',
-        desciption: 'Desenvolvido para o curso da Tybe, esse porjeto é um to-do list de browser. Pode-se criar tarefas, marca-las como feitas, apagar e salvar localmente.',
+        desciption: 'Desenvolvido para o curso da Trybe, esse projeto é um to-do list de browser. Pode-se criar tarefas, marca-las como feitas, apagar e salvar localmente.',
         technologies: ['JavaScript', 'HTML', 'CSS'],
         url: 'https://saulokirchmaier.github.io/todo_list/',
         thumbnail: 'https://i.ibb.co/C0zwqGp/to-do-list.png',
+        visit: "Acesse o site!"
       },
       {
         title: 'Pixel Art',
-        desciption: 'Desenvolvido para o curso da Tybe, Pixels Art é um projeto de criação de desenhos em pixels. podendo alterar o tamanho do quadro, gerar novas cores e escolher a quantidade de cores.',
+        desciption: 'Desenvolvido para o curso da Trybe, Pixels Art é um projeto de criação de desenhos em pixels, podendo alterar o tamanho do quadro, gerar novas cores e escolher a quantidade de cores.',
         technologies: ['JavaScript', 'HTML', 'CSS'],
         url: 'https://saulokirchmaier.github.io/pixels-art/',
         thumbnail: 'https://i.ibb.co/Ttxj2rg/pixel-art.png',
+        visit: "Acesse o site!"
       },
       {
-        title: 'Formulário',
-        desciption: 'Desenvolvido para o curso da Tybe, este formulário foi desenvolvido com Boostrap, possue uma validação de informações e um datepicker.',
-        technologies: ['JavaScript', 'HTML', 'CSS'],
+        title: 'Formulário Boostrap',
+        desciption: 'Desenvolvido para o curso da Trybe, este formulário foi desenvolvido com Boostrap, possue uma validação de informações e um datepicker.',
+        technologies: ['JavaScript', 'HTML', 'CSS', 'Boostrap'],
         url: 'https://saulokirchmaier.github.io/forms_bootstrap/',
         thumbnail: 'https://i.ibb.co/dW2bRGf/forms-bootstrap.png',
+        visit: "Acesse o site!"
       },
       {
         title: 'Shooping Cart',
-        desciption: 'Desenvolvido para o curso da Tybe, e ampliado por mim. Faz um requisição a API do mercado livre e monta uma loja de vendas com um carrinho de compras contendo os itens adicionados.',
+        desciption: 'Desenvolvido para o curso da Trybe, e ampliado por mim. Faz um requisição a API do mercado livre e monta uma loja de vendas com um carrinho de compras contendo os itens adicionados.',
         technologies: ['JavaScript', 'HTML', 'CSS'],
         url: 'https://saulokirchmaier.github.io/shopping_cart/',
         thumbnail: 'https://i.ibb.co/MBVB9pN/shopping-cart.png',
+        visit: "Acesse o site!"
       },
     ]
   }
